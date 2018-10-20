a7a50a35e7ed82a4f51d85400c4d24e3f4bbc1b3
Jason Mayberry <jasonmayberry.net@gmail.com>
2018-10-19 23:50:06 -0500

    Routing in Angular

---
+++ src/app/settings/settings.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+  routes = [
+    {linkName: 'Profile', url: 'profile'},
+    {linkName: 'Contact', url: 'contact'}
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
